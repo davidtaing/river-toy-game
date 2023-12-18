28b8c95ced3f5a732aee61e211017883f8e99c04
David Taing <dave@teeang.net>
2023-12-18 19:26:46 +1100

    feat: add naive EV calculations for Player 1

--- src/state/poc-ev-calc.ts
+++ src/state/poc-ev-calc.ts
@@ -1,6 +1,23 @@
 export const actions = { check: "X", bet: "B", fold: "F" } as const;
 
-// Naively writing an implemenation for the EV calculation of P2
+/**
+ * Naive Approach:
+ *
+ * 1. Get EV of P1 which consists of the sum of 1.1 & 1.2
+ *
+ *  1.1. Get EV of AA which consists of the sum of:
+ *    - Get EV of AA -> Bet -> Call
+ *    - Get EV of AA -> Bet -> Fold
+ *    - Get EV of AA -> Check -> Check (Ignoring this branch for now since P1 should never check)
+ *    - Get EV of AA -> Check -> Bet -> Call / Fold (Ignoring this branch for now since P1 should never check)
+ *
+ *  1.2. Get EV of QQ which consists of the sum of:
+ *    - Get EV of QQ -> Bet -> Call
+ *    - Get EV of QQ -> Bet -> Fold
+ *    - Get EV of QQ -> Check -> Check
+ *    - Get EV of QQ -> Check -> Bet -> Call / Fold (Ignoring this branch for now since P2 should never bet here)
+ */
+
 export const calcP1EV = ({
   potSize,
   betSize,
@@ -18,30 +35,60 @@ export const calcP1EV = ({
   p2Range: string[];
   board: string[];
 }) => {
-  /**
-   * For each P1 Hand
-   *  For each P1 Action
-   *    For each P2 Hand
-   *      For each P2 Action
-   */
+  const frequencyOfHand = 1 / p1Range.length;
 
-  /**
-   * Naive Approach:
-   *
-   * 1. Get EV of P1 which consists of the sum of 1.1 & 1.2
-   *
-   *  1.1. Get EV of AA which consists of the sum of:
-   *    - Get EV of AA -> Bet -> Call
-   *    - Get EV of AA -> Bet -> Fold
-   *    - Get EV of AA -> Check -> Check (Ignoring this branch for now since P1 should never check)
-   *    - Get EV of AA -> Check -> Bet -> Call / Fold (Ignoring this branch for now since P1 should never check)
-   *
-   *  1.2. Get EV of QQ which consists of the sum of:
-   *    - Get EV of QQ -> Bet -> Call
-   *    - Get EV of QQ -> Bet -> Fold
-   *    - Get EV of QQ -> Check -> Check
-   *    - Get EV of QQ -> Check -> Bet -> Call / Fold (Ignoring this branch for now since P2 should never bet here)
-   */
+  return calcP1EV_AA() * frequencyOfHand + calcP1EV_QQ() * frequencyOfHand;
+};
 
-  return 75;
+// EVs of P1's Hands
+export const calcP1EV_AA = () => {
+  return calcP1EV_AA_Bet();
+};
+
+export const calcP1EV_QQ = () => {
+  const bluffFreq = 0.5;
+  return caclP1EV_QQ_Bet() * bluffFreq + calcP1EV_QQ_Check() * (1 - bluffFreq);
+};
+
+// P1 Hand -> P1 Actions
+export const calcP1EV_AA_Bet = () => {
+  const callFreq = 0.5;
+  return (
+    calcP1EV_AA_Bet_Call() * callFreq + calcP1EV_AA_Bet_Fold() * (1 - callFreq)
+  );
+};
+
+export const caclP1EV_QQ_Bet = () => {
+  const callFreq = 0.5;
+  return (
+    calcP1EV_QQ_Bet_Call() * (1 - callFreq) + calcP1EV_QQ_Bet_Fold() * callFreq
+  );
+};
+
+export const calcP1EV_QQ_Check = () => {
+  return 0;
+};
+
+// P1 Actions -> P2 Responses (Actions)
+export const calcP1EV_AA_Bet_Call = () => {
+  const potSize = 100;
+  const betSize = 100;
+
+  const winAmount = potSize + betSize;
+  return winAmount;
+};
+
+export const calcP1EV_AA_Bet_Fold = () => {
+  const potSize = 100;
+  return potSize;
+};
+
+export const calcP1EV_QQ_Bet_Call = () => {
+  const betSize = 100;
+  return -betSize;
+};
+
+export const calcP1EV_QQ_Bet_Fold = () => {
+  const potSize = 100;
+  return potSize;
 };
