2a5729a7369943b77db1e6618496ccb2fe258a52
David Taing <dave@teeang.net>
2023-12-18 21:38:24 +1100

    test: add tests for P1 EV calculations

--- src/state/tests/poc-ev-calc.test.ts
+++ src/state/tests/poc-ev-calc.test.ts
@@ -1,4 +1,4 @@
-import { calcP1EV, calcP2EV } from "../poc-ev-calc";
+import { calcP1EV, calcP1EV_QQ, calcP2EV } from "../poc-ev-calc";
 
 describe("Calculate P1's EV", () => {
   it("returns EV of 75 for a standard toy-game", () => {
@@ -12,6 +12,69 @@ describe("Calculate P1's EV", () => {
       board: ["3c", "3s", "3d", "2d", "2h"],
     });
     const expected = 75;
+    expect(actual).toEqual(expected);
+  });
+
+  it("returns EV of 83.33 for a 2x overbet toy-game", () => {
+    const actual = calcP1EV({
+      potSize: 100,
+      betSize: 200,
+      bluffFreq: 2 / 3,
+      callFreq: 1 / 3,
+      p1Range: ["AA", "QQ"],
+      p2Range: ["KK"],
+      board: ["3c", "3s", "3d", "2d", "2h"],
+    });
+    const expected = 83.33;
+    expect(actual).toEqual(expected);
+  });
+
+  it("returns EV of 87.5 for a 3x overbet toy-game", () => {
+    const actual = calcP1EV({
+      potSize: 100,
+      betSize: 300,
+      bluffFreq: 3 / 4,
+      callFreq: 1 / 4,
+      p1Range: ["AA", "QQ"],
+      p2Range: ["KK"],
+      board: ["3c", "3s", "3d", "2d", "2h"],
+    });
+    const expected = 87.5;
+    expect(actual).toEqual(expected);
+  });
+});
+
+describe("Calculate P1's EV of holding QQ", () => {
+  test("EV is 0 for a standard toy-game", () => {
+    const actual = calcP1EV_QQ({
+      potSize: 100,
+      betSize: 100,
+      bluffFreq: 0.5,
+      callFreq: 0.5,
+    });
+    const expected = 0;
+    expect(actual).toEqual(expected);
+  });
+
+  test("EV is 0 for a 2x overbet toy-game", () => {
+    const actual = calcP1EV_QQ({
+      potSize: 100,
+      betSize: 200,
+      bluffFreq: 2 / 3,
+      callFreq: 1 / 3,
+    });
+    const expected = 0;
+    expect(actual).toEqual(expected);
+  });
+
+  test("EV is 0 for a 3x overbet toy-game", () => {
+    const actual = calcP1EV_QQ({
+      potSize: 100,
+      betSize: 300,
+      bluffFreq: 3 / 4,
+      callFreq: 1 / 4,
+    });
+    const expected = 0;
     expect(actual).toEqual(expected);
   });
 });
